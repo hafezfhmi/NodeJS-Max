446dbf483a104c7002539b8a1c5926345191b9f2
hafezfhmi <hafezfahmi2000@gmail.com>
2022-03-30 14:56:09 +0800

    3.9 Routing Requests

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  //   process.exit();
+  const url = req.url;
+
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Submit</button></form></body>'
+    );
+    res.write('</html>');
+
+    return res.end();
+  }
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head></head>');
