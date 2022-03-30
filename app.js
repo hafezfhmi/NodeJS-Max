72b3c5caac223a739c9b529433660c9b2fd74086
hafezfhmi <hafezfahmi2000@gmail.com>
2022-03-30 15:11:25 +0800

    3.10 Redirecting Requests

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
 
   if (url === '/') {
     res.setHeader('Content-Type', 'text/html');
@@ -12,6 +14,13 @@ const server = http.createServer((req, res) => {
     );
     res.write('</html>');
 
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
     return res.end();
   }
 
