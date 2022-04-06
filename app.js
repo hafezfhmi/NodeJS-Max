c05775b329269a75a4532fd5ed72ff8de408599b
hafezfhmi <hafezfahmi2000@gmail.com>
2022-04-06 15:43:04 +0800

    5.12 Adding a 404 Error Page

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use(express.urlencoded(false));
 app.use(adminRouter);
 app.use(shopRouter);
 
+app.use((req, res, send) => {
+  res.status(404).send('Sorry, cant find that page');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
