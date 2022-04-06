2c0b57556280250a757306d16334122a03ff2776
hafezfhmi <hafezfahmi2000@gmail.com>
2022-04-06 16:02:40 +0800

    5.18 Styling our Pages, 5.19 Serving Files Statistically

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const http = require('http');
+const path = require('path');
 const express = require('express');
 const app = express();
 
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
+const rootDir = require('./utils/path');
 
 app.use(express.urlencoded(false));
+app.use(express.static(path.join(rootDir, 'public')));
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
