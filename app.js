ac3410f4e59d87c018d2ec7ad805c33c755dad0b
hafezfhmi <hafezfahmi2000@gmail.com>
2022-04-06 15:06:06 +0800

    5.7 Handling Different Routes

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Hello World');
+  console.log('Always run');
   next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
   console.log('Hello World2');
-  next();
+  res.send('add-product');
+});
+
+app.use('/', (req, res, next) => {
+  console.log('Hello World2');
+  res.send({ some: 'json' });
 });
 
 const server = http.createServer(app);
