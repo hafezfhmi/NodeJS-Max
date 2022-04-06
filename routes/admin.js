f3acc6b9a797826a0949563b4287591e90cd03bc
hafezfhmi <hafezfahmi2000@gmail.com>
2022-04-06 15:43:49 +0800

    5.13 Filtering Paths

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,11 @@ const adminRouter = require('express').Router();
 
 adminRouter.get('/add-product', (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit"/>Submit</button></form>'
+    '<form action="/add-product" method="POST"><input type="text" name="title"><button type="submit"/>Submit</button></form>'
   );
 });
 
-adminRouter.post('/product', (req, res, next) => {
+adminRouter.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
